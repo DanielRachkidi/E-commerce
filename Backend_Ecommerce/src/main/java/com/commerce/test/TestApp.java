a35c4522524b375c4c105edbee65cb4ecdb5d847
daniel <daniel@rachkidi.com>
2023-04-04 20:12:05 +0300

    first

--- Backend_Ecommerce/src/main/java/com/commerce/test/TestApp.java
+++ Backend_Ecommerce/src/main/java/com/commerce/test/TestApp.java
@@ -2,7 +2,7 @@ package com.commerce.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//jkhkjhjhjhg
 @SpringBootApplication
 public class TestApp
 {
